0950bd006842091a0587c273586d7449a7c994b7
boulch <christophe.boulanger@imio.be>
2015-08-10 16:04:09 +0200

    Add folders and events creation in testing.py (not directly created in .robot

--- Solgema/fullcalendar/testing.py
+++ Solgema/fullcalendar/testing.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
+
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 
+from plone.app.testing import setRoles
+from plone.app.testing import login
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+
 from plone.testing import z2
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
-
+import transaction
 
 class Fixture(PloneSandboxLayer):
 
@@ -21,6 +28,46 @@ class Fixture(PloneSandboxLayer):
     def setUpPloneSite(self, portal):
         # Install into Plone site using portal_setup
         self.applyProfile(portal, 'Solgema.fullcalendar:default')
+
+        # Login and create some test content
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
+
+        folder_one = api.content.create(
+            type='Folder',
+            id='folder_one',
+            container=portal,
+        )
+
+        event_one = api.content.create(
+            type='Event',
+            id='event_one',
+            title='Event One',
+            container=folder_one,
+        )
+
+        event_two = api.content.create(
+            type='Event',
+            id='event_two',
+            title='Event Two',
+            container=folder_one,
+        )
+
+        folder_two =  api.content.create(
+            type='Folder',
+            id='folder_two',
+             container=folder_one,
+        )
+        
+        collection_one = api.content.create(
+            type='Collection',
+            id='collection_one',
+            title='Collection One',
+            container=folder_two,
+        )
+
+        transaction.commit()
+
 
 FIXTURE = Fixture()
 INTEGRATION_TESTING = IntegrationTesting(
